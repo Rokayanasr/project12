c3edbd37e28f627b234d6b5dd1c971aa302b4fe7
Rokaya Nasr <rokanasr60@gmail.com>
2024-06-18 01:51:26 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        violet: '#8668ff',
+        yellow: '#ffd360',
+        grey: '#212121',
+        white: '#ffffff'
+            },
+    },
+  },
+  plugins: [],
+};
